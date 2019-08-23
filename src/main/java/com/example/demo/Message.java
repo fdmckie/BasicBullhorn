43a1f5391f73ebf8600af4669e728231f019c8ce
fdmckie <fdmckie@gmail.com>
2019-08-23 15:00:21 -0400

    Basic Bullhorn - Step 1

---
+++ src/main/java/com/example/demo/Message.java
@@ -0,0 +1,85 @@
+package com.example.demo;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Entity
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Size(min=1)
+    private String title;
+
+    @NotNull
+    @Size(min=1)
+    private String content;
+
+    @NotNull
+    @Size(min=1)
+    private String sentby;
+
+    @NotNull
+    private LocalDate posteddate;
+
+    @NotNull
+    private LocalTime postedtime;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getSentby() {
+        return sentby;
+    }
+
+    public void setSentby(String sentby) {
+        this.sentby = sentby;
+    }
+
+    public LocalDate getPosteddate() {
+        return posteddate;
+    }
+
+    public void setPosteddate(LocalDate posteddate) {
+        this.posteddate = LocalDate.now();
+    }
+
+    public LocalTime getPostedtime() {
+        return postedtime;
+    }
+
+    public void setPostedtime(LocalTime postedtime) {
+        this.postedtime = LocalTime.now();
+    }
+}
